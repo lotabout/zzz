b83a7509edc62c799034430bc4f01fff20a6f529
Mark Wallace <lotabout@gmail.com>
2015-03-11 10:31:10 +0800

    [index] index page configuration done.

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -39,9 +39,34 @@ jQuery(function($) {
         $(window).scroll(refreshNav);
     }
 
+
+    /* Load theme */
+    function loadTheme(html) {
+      var tag, theme, href,
+          $css_theme = $('#css_theme');
+      if (html) {
+         tag = $('#tag-theme', html);
+      } else {
+         tag = $('#tag-theme');
+      }
+
+
+      if (tag.length == 1) {
+        theme  = tag.attr('data-theme');
+        href = '/assets/css/theme.'+theme+'.css';
+      } else {
+        href = '/assets/css/theme.gray.css';
+      }
+      if ($css_theme.attr('href') !== href ) {
+          $css_theme.attr('href', href);
+      }
+    }
+
     $(document).ready(function() {
         //$(".body").fitVids();
         headerNav();
+        loadTheme();
+        console.log("HHHHHHHHHH");
         // $('#history-back').on('click',function(e){
         //     e.preventDefault();
         //     window.History.back();
